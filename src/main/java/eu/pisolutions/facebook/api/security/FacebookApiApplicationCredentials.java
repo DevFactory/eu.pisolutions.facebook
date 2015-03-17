ad773707312bc85c816623ed7836864cdb9594b0
Laurent Pireyn <laurent.pireyn@pisolutions.eu>
2015-03-17 15:30:13 +0100

    Implement equals, hashCode and toString in FacebookApiApplicationCredentials

--- src/main/java/eu/pisolutions/facebook/api/security/FacebookApiApplicationCredentials.java
+++ src/main/java/eu/pisolutions/facebook/api/security/FacebookApiApplicationCredentials.java
@@ -25,4 +25,26 @@ public final class FacebookApiApplicationCredentials extends FacebookApiCredenti
     public String getSecret() {
         return secret;
     }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (!(object instanceof FacebookApiApplicationCredentials)) {
+            return false;
+        }
+        final FacebookApiApplicationCredentials other = (FacebookApiApplicationCredentials) object;
+        return id == other.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Long.valueOf(id).hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "Facebook API application credentials (ID: " + id + ')';
+    }
 }
